f6c9221d9e0406e63e941920bc172dabd2078233
Paul Lee <paul@techiesapp.io>
2024-10-04 09:55:56 +0800

    updated

--- lib/mysql.ts
+++ lib/mysql.ts
@@ -4,7 +4,7 @@ import mysql from "mysql2/promise";
 const connection = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "0199510532",
+  password: "",
   database: "nova-test",
 });
 
