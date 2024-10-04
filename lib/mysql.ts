3d85964f30f4924ae9c167154038e85bd32ae2ef
Paul Lee <paul@techiesapp.io>
2024-10-04 09:58:11 +0800

    added dot.env

--- lib/mysql.ts
+++ lib/mysql.ts
@@ -1,11 +1,13 @@
-// create mysql connection for export
+import dotenv from "dotenv";
 import mysql from "mysql2/promise";
 
+dotenv.config();
+
 const connection = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nova-test",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default connection;
