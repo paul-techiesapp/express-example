1b665469908d440b7fe5c39976b55005e7a358d8
Paul Lee <paul@techiesapp.io>
2025-01-21 15:22:55 +0800

    updated

--- controllers/video.controller.ts
+++ controllers/video.controller.ts
@@ -1,15 +1,57 @@
 import { Request, Response } from "express";
 import * as _ from "lodash";
-import videoData from "../DATA/video";
+import pool from "../db";
 
 class VideoController {
-  public getVideos(req: Request, res: Response): void {
-    res.json(videoData);
+  public async getVideos(req: Request, res: Response) {
+    const connection = await pool.getConnection();
+    try {
+      const [videos] = await connection.query("SELECT * FROM videos");
+      res.json(videos);
+    } finally {
+      connection.release();
+    }
   }
 
-  public getVideo(req: Request, res: Response): void {
-    const video = _.find(videoData, { id: Number(req.params.id) });
-    res.json(video);
+  public async getOneVideo(req: Request, res: Response) {
+    const connection = await pool.getConnection();
+    try {
+      const [video] = await connection.query(
+        `SELECT * FROM videos WHERE id = ?`,
+        [req.params.id]
+      );
+      res.json(video);
+    } finally {
+      connection.release();
+    }
+  }
+
+  public async createVideo(req: Request, res: Response) {
+    const connection = await pool.getConnection();
+
+    const newVideo = req.body;
+    await connection.query("INSERT INTO videos SET ?", newVideo);
+    res.json({ message: "Video saved" });
+  }
+
+  public async updateVideo(req: Request, res: Response) {
+    const connection = await pool.getConnection();
+
+    const updatedVideo = req.body;
+    const id = req.params.id;
+    await connection.query("UPDATE videos SET ? WHERE id = ?", [
+      updatedVideo,
+      id,
+    ]);
+    res.json({ message: "Video updated" });
+  }
+
+  public async deleteVideo(req: Request, res: Response) {
+    const connection = await pool.getConnection();
+
+    const id = req.params.id;
+    await connection.query("DELETE FROM videos WHERE id = ?", [id]);
+    res.json({ message: "Video deleted" });
   }
 }
 
--- routes/video.routes.ts
+++ routes/video.routes.ts
@@ -4,6 +4,9 @@ import VideoController from "../controllers/video.controller";
 const router = express.Router();
 
 router.get("/", VideoController.getVideos);
-router.get("/:id", VideoController.getVideo);
+router.get("/:id", VideoController.getOneVideo);
+router.post("/", VideoController.createVideo);
+router.put("/:id", VideoController.updateVideo);
+router.delete("/:id", VideoController.deleteVideo);
 
 export default router;
